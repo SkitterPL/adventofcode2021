6984f198cd14f576f5cf1e0056d8a415d9b8d5e6
SkitterPL <bartosz.luczak@tsh.io>
2021-12-02 06:28:17 +0100

    Day 2

--- day2.go
+++ day2.go
@@ -1,7 +1,67 @@
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
 //https://adventofcode.com/2021/day/2
 
+type Position struct {
+	Horizontal int
+	Depth      int
+	Aim        int
+}
+
+const (
+	Forward string = "forward"
+	Down    string = "down"
+	Up      string = "up"
+)
+
 func day2() (int, int) {
-	return 0, 0
+	data := fileToStringArray("input/2/input.txt")
+	position, positionWithAim := calculatePosition(data)
+	return position.result(), positionWithAim.result()
+}
+
+func calculatePosition(data []string) (*Position, *Position) {
+	var position, positionWithAim Position = Position{0, 0, 0}, Position{0, 0, 0}
+
+	for _, line := range data {
+		splittedDirectionAndValue := strings.Fields(line)
+		direction := splittedDirectionAndValue[0]
+		convertedValue, _ := strconv.Atoi(splittedDirectionAndValue[1])
+		position.move(direction, convertedValue)
+		positionWithAim.moveUsingAim(direction, convertedValue)
+	}
+
+	return &position, &positionWithAim
+}
+
+func (position *Position) move(direction string, value int) {
+	switch direction {
+	case Forward:
+		position.Horizontal += value
+	case Down:
+		position.Depth += value
+	case Up:
+		position.Depth -= value
+	}
+}
+
+func (position *Position) moveUsingAim(direction string, value int) {
+	switch direction {
+	case Forward:
+		position.Horizontal += value
+		position.Depth += position.Aim * value
+	case Down:
+		position.Aim += value
+	case Up:
+		position.Aim -= value
+	}
+}
+
+func (position *Position) result() int {
+	return position.Depth * position.Horizontal
 }
