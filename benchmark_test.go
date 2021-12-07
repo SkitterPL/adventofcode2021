914a15c94f252e2bab220aefb78751b8e6d91a3b
SkitterPL <bartosz.luczak@tsh.io>
2021-12-07 08:06:53 +0100

    Add benchmark

---
+++ benchmark_test.go
@@ -0,0 +1,10 @@
+package main
+
+import "testing"
+
+//go test -count 5 -run '^$' -bench . -memprofile=v1.mem.pprof -cpuprofile=v1.cpu.pprof > v1.txt
+func BenchmarkAdventOfCode(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		day7()
+	}
+}
