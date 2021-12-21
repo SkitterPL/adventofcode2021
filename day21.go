75e013a8238bfdbf57c16407601cf2fa7065b1cb
SkitterPL <bartosz.luczak@tsh.io>
2021-12-21 16:52:25 +0100

    Part 2 Day 21

--- day21.go
+++ day21.go
@@ -21,12 +21,28 @@ type DeterministicDice struct {
 	rollCount    int
 }
 
+func (dice *DeterministicDice) roll() int {
+	dice.currentValue++
+	if dice.currentValue > 100 {
+		dice.currentValue = 1
+	}
+	dice.rollCount++
+	return dice.currentValue
+}
+
 func day21() (int, int) {
-	data := fileToStringArray("input/21/test_input.txt")
+	mostWins := 0
+	data := fileToStringArray("input/21/input.txt")
 	player1Position, _ := strconv.Atoi(strings.Fields(data[0])[4])
 	player2Position, _ := strconv.Atoi(strings.Fields(data[1])[4])
 	losingPlayerScore, diceRolls := playWithDeterministicDice(&Player{player1Position, 0}, &Player{player2Position, 0})
-	return losingPlayerScore * diceRolls, 0
+	player1Wins, player2Wins := playWithDiracDice(Player{player1Position, 0}, Player{player2Position, 0}, 1, true)
+	if player1Wins > player2Wins {
+		mostWins = player1Wins
+	} else {
+		mostWins = player2Wins
+	}
+	return losingPlayerScore * diceRolls, mostWins
 }
 
 func playWithDeterministicDice(player1 *Player, player2 *Player) (int, int) {
@@ -58,11 +74,45 @@ func playWithDeterministicDice(player1 *Player, player2 *Player) (int, int) {
 	return losingPoints, dice.rollCount
 }
 
-func (dice *DeterministicDice) roll() int {
-	dice.currentValue++
-	if dice.currentValue > 100 {
-		dice.currentValue = 1
+//3 1+1+1
+//4 1+1+2 1+2+1 2+1+1
+//5 1+2+2 1+1+3 1+3+1 3+1+1 2+1+2 2+2+1
+//6 2+2+2 1+2+3 3+2+1 2+3+1 2+1+3 1+3+2 3+1+2
+//7 1+3+3 3+1+3 3+3+1 2+2+3 2+3+2 3+2+2
+//8 3+3+2 2+3+3 3+2+3
+//9 3+3+3
+func playWithDiracDice(player1 Player, player2 Player, universes int, player1Moves bool) (int, int) {
+	possibleDiracDiceRollResults := map[int]int{3: 1, 4: 3, 5: 6, 6: 7, 7: 6, 8: 3, 9: 1}
+	if player1.score >= 21 {
+		return universes, 0
+	} else if player2.score >= 21 {
+		return 0, universes
 	}
-	dice.rollCount++
-	return dice.currentValue
+
+	var currentPlayer Player
+	var player1Wins, player2Wins, subPlayer1Wins, subPlayer2Wins int
+
+	for rollSum, possibilities := range possibleDiracDiceRollResults {
+		if player1Moves {
+			currentPlayer = player1
+		} else {
+			currentPlayer = player2
+		}
+		currentPlayer.boardPosition = (currentPlayer.boardPosition + rollSum%10) % 10
+		if currentPlayer.boardPosition == 0 {
+			currentPlayer.boardPosition = 10
+		}
+		currentPlayer.score += currentPlayer.boardPosition
+
+		if player1Moves {
+			subPlayer1Wins, subPlayer2Wins = playWithDiracDice(currentPlayer, player2, universes*possibilities, !player1Moves)
+		} else {
+			subPlayer1Wins, subPlayer2Wins = playWithDiracDice(player1, currentPlayer, universes*possibilities, !player1Moves)
+		}
+
+		player1Wins += subPlayer1Wins
+		player2Wins += subPlayer2Wins
+	}
+
+	return player1Wins, player2Wins
 }
